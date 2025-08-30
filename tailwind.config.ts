80a5e6ea62ce37601a747e18eb4d21572446f9fb
kimjk4 <82184157+kimjk4@users.noreply.github.com>
2025-08-29 20:21:53 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from "tailwindcss";
 const config: Config = {
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}"
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
   plugins: [],
 };
